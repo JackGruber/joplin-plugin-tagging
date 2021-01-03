f06246de084d22768373ebb49bd1e77cefa9a840
JackGruber <24863925+JackGruber@users.noreply.github.com>
2021-01-03 12:21:31 +0100

    Correct checkbox toggel

--- src/webview.js
+++ src/webview.js
@@ -8,21 +8,39 @@ function SetCheckboxIndeterminate(){
 }
 
 function ToggleTagCheckbox(element) {
-    if(element.value == 1) {
-        element.indeterminate = true;
-        element.checked = false;
-        element.value = 2;
+    if(element.className.indexOf('indeterminate') != -1)
+    {
+        if(element.value == 1) {
+            element.indeterminate = true;
+            element.checked = false;
+            element.value = 2;
+        }
+        else if(element.value == 2) {
+            element.indeterminate = false;
+            element.checked = false;
+            element.value = 0;
+        }
+        else {
+            element.indeterminate = false;
+            element.checked = true
+            element.value = 1;
+        }
     }
-    else if(element.value == 2) {
-        element.indeterminate = false;
-        element.checked = false;
-        element.value = 0;
+    else
+    {
+        if(element.value == 1) {
+            element.checked = false
+            element.value = 0;
+        } else {
+            element.checked = true
+            element.value = 1;
+        }
     }
-    else {
-        element.indeterminate = false;
-        element.checked = true
-        element.value = 1;
-    }
+
     return false;
 }
 
+function SaveCheckboxState()
+{
+
+}
