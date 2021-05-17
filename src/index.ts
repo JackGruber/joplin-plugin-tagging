0349c63541f0b27fedb096a23ca8d78be471f2be
JackGruber <24863925+JackGruber@users.noreply.github.com>
2021-05-17 19:14:14 +0200

    Code format

--- src/index.ts
+++ src/index.ts
@@ -49,15 +49,15 @@ joplin.plugins.register({
       MenuItemLocation.Tools
     );
     await joplin.views.menuItems.create(
-      'contextMenuItemCopyAllTags', 
-      'CopyAllTags', 
+      "contextMenuItemCopyAllTags",
+      "CopyAllTags",
       MenuItemLocation.NoteListContextMenu
     );
 
-    const tag_dialog = await joplin.views.dialogs.create('TagDialog');
-    await joplin.views.dialogs.addScript(tag_dialog, 'webview.js');
-    await joplin.views.dialogs.addScript(tag_dialog, 'webview.css')
-    
+    const tag_dialog = await joplin.views.dialogs.create("TagDialog");
+    await joplin.views.dialogs.addScript(tag_dialog, "webview.js");
+    await joplin.views.dialogs.addScript(tag_dialog, "webview.css");
+
     await joplin.commands.register({
       name: "TaggingDialog",
       label: "Tagging list",
@@ -70,19 +70,23 @@ joplin.plugins.register({
           for (let ids_pos = 0; ids_pos < ids.length; ids_pos++) {
             var pageNum = 1;
             do {
-              var tags = await joplin.data.get(["notes", ids[ids_pos], "tags"], {
-                fields: "id, title",
-                limit: 20,
-                page: pageNum++,
-              });
+              var tags = await joplin.data.get(
+                ["notes", ids[ids_pos], "tags"],
+                {
+                  fields: "id, title",
+                  limit: 20,
+                  page: pageNum++,
+                }
+              );
               for (var tags_pos = 0; tags_pos < tags.items.length; tags_pos++) {
-                var tag_id = tags['items'][tags_pos]['id'];
-                if(typeof tags_on_notes[tag_id] === 'undefined'){
-                  tags_on_notes[tag_id] = {}
-                  tags_on_notes[tag_id]['count'] = 1;
-                  tags_on_notes[tag_id]['title'] = tags['items'][tags_pos]['title'];
+                var tag_id = tags["items"][tags_pos]["id"];
+                if (typeof tags_on_notes[tag_id] === "undefined") {
+                  tags_on_notes[tag_id] = {};
+                  tags_on_notes[tag_id]["count"] = 1;
+                  tags_on_notes[tag_id]["title"] =
+                    tags["items"][tags_pos]["title"];
                 } else {
-                  tags_on_notes[tag_id]['count'] ++;
+                  tags_on_notes[tag_id]["count"]++;
                 }
               }
             } while (tags.has_more);
@@ -91,46 +95,49 @@ joplin.plugins.register({
           // create tagging list
           let tags_status = [];
           const tag_list = [];
-          for(var key in tags_on_notes){
-            if(tags_on_notes[key]['count'] == ids.length) {
-              tags_status[key] = 1
-              tag_list.push(`<input type="checkbox" value="1" onclick="ToggleTagCheckbox(this, '${key}')" checked="checked" /> ${tags_on_notes[key]['title']} <br>`)
-              tag_list.push(`<input type="hidden" name="${key}" value="1">`)
+          for (var key in tags_on_notes) {
+            if (tags_on_notes[key]["count"] == ids.length) {
+              tags_status[key] = 1;
+              tag_list.push(
+                `<input type="checkbox" value="1" onclick="ToggleTagCheckbox(this, '${key}')" checked="checked" /> ${tags_on_notes[key]["title"]} <br>`
+              );
+              tag_list.push(`<input type="hidden" name="${key}" value="1">`);
             } else {
-              tags_status[key] = 2
-              tag_list.push(`<input type="checkbox" value="2" onclick="ToggleTagCheckbox(this, '${key}')" class="indeterminate" /> ${tags_on_notes[key]['title']} <br>`)
-              tag_list.push(`<input type="hidden" name="${key}" value="2">`)
+              tags_status[key] = 2;
+              tag_list.push(
+                `<input type="checkbox" value="2" onclick="ToggleTagCheckbox(this, '${key}')" class="indeterminate" /> ${tags_on_notes[key]["title"]} <br>`
+              );
+              tag_list.push(`<input type="hidden" name="${key}" value="2">`);
             }
           }
 
-          await joplin.views.dialogs.setHtml(tag_dialog, `
+          await joplin.views.dialogs.setHtml(
+            tag_dialog,
+            `
           <div id="copytags">
             <form name="tags">
-            ${tag_list.join('\n')}
+            ${tag_list.join("\n")}
             </form>
           </div>
-          `);
+          `
+          );
           const result = await joplin.views.dialogs.open(tag_dialog);
 
-          // process with tagging 
-          if(result['id'] == 'ok') {
-            for(var key in result['formData']['tags']){
-              if(result['formData']['tags'][key] != tags_status[key]) {
-                if(result['formData']['tags'][key] == 0){
+          // process with tagging
+          if (result["id"] == "ok") {
+            for (var key in result["formData"]["tags"]) {
+              if (result["formData"]["tags"][key] != tags_status[key]) {
+                if (result["formData"]["tags"][key] == 0) {
                   // Remove Tag
                   for (var i = 0; i < ids.length; i++) {
-                    await joplin.data.delete(
-                      ["tags", key, "notes", ids[i]]
-                    );
+                    await joplin.data.delete(["tags", key, "notes", ids[i]]);
                   }
-                } else if(result['formData']['tags'][key] == 1){
+                } else if (result["formData"]["tags"][key] == 1) {
                   // Add Tag
                   for (var i = 0; i < ids.length; i++) {
-                    await joplin.data.post(
-                      ["tags", key, "notes"],
-                      null,
-                      { id: ids[i] }
-                    );
+                    await joplin.data.post(["tags", key, "notes"], null, {
+                      id: ids[i],
+                    });
                   }
                 }
               }
@@ -146,8 +153,8 @@ joplin.plugins.register({
       MenuItemLocation.Tools
     );
     await joplin.views.menuItems.create(
-      'contextMenuItemTaggingDialog', 
-      'TaggingDialog', 
+      "contextMenuItemTaggingDialog",
+      "TaggingDialog",
       MenuItemLocation.NoteListContextMenu
     );
   },
