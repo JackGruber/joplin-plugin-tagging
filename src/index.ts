fa2473af41b2bd8415045f6a9f27dd91960f8156
JackGruber <24863925+JackGruber@users.noreply.github.com>
2021-05-18 07:55:08 +0200

    move copyAllTags

--- src/index.ts
+++ src/index.ts
@@ -10,37 +10,7 @@ joplin.plugins.register({
       name: "CopyAllTags",
       label: "Copy all Tags",
       enabledCondition: "noteListHasNotes",
-      execute: async () => {
-        var noteIds = await joplin.workspace.selectedNoteIds();
-        if (noteIds.length > 1) {
-          const note = await joplin.data.get(["notes", noteIds[0]], {
-            fields: ["id", "title"],
-          });
-          if (
-            (await joplin.views.dialogs.showMessageBox(
-              "Copy all tags from " + note["title"] + " ?"
-            )) == 0
-          ) {
-            var pageNum = 1;
-            do {
-              var tags = await joplin.data.get(["notes", noteIds[0], "tags"], {
-                fields: "id",
-                limit: 10,
-                page: pageNum++,
-              });
-              for (var a = 0; a < tags.items.length; a++) {
-                for (var i = 1; i < noteIds.length; i++) {
-                  await joplin.data.post(
-                    ["tags", tags.items[a].id, "notes"],
-                    null,
-                    { id: noteIds[i] }
-                  );
-                }
-              }
-            } while (tags.has_more);
-          }
-        }
-      },
+      execute: async () => tagging.copyAllTags() ,
     });
 
     await joplin.views.menuItems.create(
--- src/tagging.ts
+++ src/tagging.ts
@@ -1,6 +1,39 @@
+import joplin from "api";
+
 export namespace tagging {
   export async function processDialogMsg(msg) {
     console.log(`on message: ${JSON.stringify(msg)}`);
   }
 
+  export async function copyAllTags() {
+    var noteIds = await joplin.workspace.selectedNoteIds();
+    if (noteIds.length > 1) {
+      const note = await joplin.data.get(["notes", noteIds[0]], {
+        fields: ["id", "title"],
+      });
+      if (
+        (await joplin.views.dialogs.showMessageBox(
+          "Copy all tags from " + note["title"] + " ?"
+        )) == 0
+      ) {
+        var pageNum = 1;
+        do {
+          var tags = await joplin.data.get(["notes", noteIds[0], "tags"], {
+            fields: "id",
+            limit: 10,
+            page: pageNum++,
+          });
+          for (var a = 0; a < tags.items.length; a++) {
+            for (var i = 1; i < noteIds.length; i++) {
+              await joplin.data.post(
+                ["tags", tags.items[a].id, "notes"],
+                null,
+                { id: noteIds[i] }
+              );
+            }
+          }
+        } while (tags.has_more);
+      }
+    }
+  }
 }
